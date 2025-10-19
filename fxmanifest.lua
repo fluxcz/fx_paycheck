ea0efd3a3c51408197e86c66f70f0db718813b12
Flux <flux@whsrp.eu>
2025-10-19 22:33:30 +0200

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Flux'
+github 'https://github.com/fluxcz'
+description 'FX Paycheck | Flux Development'
+
+shared_script 'config.lua'
+server_script 'game/src/sv/sv.lua'
+client_script 'game/src/cl/cl.lua'
+
