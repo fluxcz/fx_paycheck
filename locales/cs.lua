b4a839ec41ec1e94c43603ae6a754c9c562b94ea
Flux <flux@whsrp.eu>
2025-12-11 22:33:46 +0100

    cs

---
+++ locales/cs.lua
@@ -0,0 +1,21 @@
+Locales['cs'] = {
+    ['received_bank'] = 'Obdržel jsi: ~g~$%s',
+    ['received_cash'] = 'Obdržel jsi: ~g~$%s',
+    ['received_item'] = 'Obdržel jsi: ~g~%s',
+    ['base_salary'] = 'Základní plat: $%s',
+    ['tax'] = '~n~Daň (%s%%) - $%s',
+    
+    -- ox_lib specific
+    ['lib_received_bank'] = '\n\nObdržel jsi: $%s',
+    ['lib_received_cash'] = '\n\nObdržel jsi: $%s',
+    ['lib_received_item'] = '\n\nObdržel jsi: $%s',
+    ['lib_base_salary'] = 'Základní plat: $%s',
+    ['lib_tax'] = '\nDaň (%s%%): -$%s',
+    ['lib_title'] = 'Výplata přijata',
+
+    -- other
+    ['other_received_bank'] = '\n\nUloženo v bance: $%s',
+    ['other_received_cash'] = '\n\nObdrženo v hotovosti: $%s',
+    ['other_received_item'] = '\n\nObdržen předmět: %s',
+    ['other_tax'] = '\nDaň (%s%%): -$%s',
+}
