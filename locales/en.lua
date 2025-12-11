93209028c09bc41d3c35a079a0efb7dd4aa8727d
Flux <flux@whsrp.eu>
2025-12-11 22:34:04 +0100

    en

---
+++ locales/en.lua
@@ -0,0 +1,21 @@
+Locales['en'] = {
+    ['received_bank'] = 'You received: ~g~$%s',
+    ['received_cash'] = 'You received: ~g~$%s',
+    ['received_item'] = 'You received: ~g~%s',
+    ['base_salary'] = 'Base Salary: $%s',
+    ['tax'] = '~n~Tax (%s%%) - $%s',
+    
+    -- ox_lib specific
+    ['lib_received_bank'] = '\n\nYou received: $%s',
+    ['lib_received_cash'] = '\n\nYou received: $%s',
+    ['lib_received_item'] = '\n\nYou received: $%s',
+    ['lib_base_salary'] = 'Base Salary: $%s',
+    ['lib_tax'] = '\nTax (%s%%): -$%s',
+    ['lib_title'] = 'Paycheck Received',
+
+    -- other
+    ['other_received_bank'] = '\n\nDeposited to bank: $%s',
+    ['other_received_cash'] = '\n\nReceived in cash: $%s',
+    ['other_received_item'] = '\n\nReceived item: %s',
+    ['other_tax'] = '\nTax (%s%%): -$%s',
+}
