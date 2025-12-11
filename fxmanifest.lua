6d9cfd39c775c12a367092aa54f4122ef73ed54d
Flux <flux@whsrp.eu>
2025-12-11 22:33:04 +0100

    1.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,16 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 author 'Flux'
 github 'https://github.com/fluxcz'
 description 'FX Paycheck | Flux Development'
+version '1.2'
 
-shared_script 'config.lua'
+shared_scripts {
+    'config.lua',
+    'locales/*.lua',
+    '@oxmysql/lib/MySQL.lua'
+}
 server_script 'game/src/sv/sv.lua'
 client_script 'game/src/cl/cl.lua'
 
